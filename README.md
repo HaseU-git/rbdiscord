2e6db8eb7f61af419bb35dc2f45ea4b611b1a4e0
HaseU-git <hasegawayuta@MacBook-Pro-3.local>
2021-03-09 19:41:34 +0900

    fix README

--- README.md
+++ README.md
@@ -22,8 +22,122 @@ rubyを用いたプログラムを書いた経験がなかったため、rubyで
 まず、discordのアカウントを作成します。  
 次に、自分が管理者権限を持っているサーバーをdiscord上で作成します。  
 そして、botを作成し管理者権限のあるサーバーに招待していきます。  
+以下の手順に従ってdiscordのbotを作成してください。  
+
+[こちら](https://discord.com/developers/applications)にアクセスし、画面右上の`New Application`ボタンを押してください。  
+ログインを要求された場合には、ご自身のdiscordアカウント情報を入力し、ログインを済ませてください。  
+
+![写真1](images/image1.png)
+
+botの名前を決める画面が表示されますので、お好きな名前を入力し次に進んでください。  
+入力が完了すると、下のような画面になります。  
+
+![写真2](images/image2.png)
+
+画面左側のメニューバーから「bot」を選択してください。  
+
+![写真3](images/image3.png)
+
+すると、下のような画面になるので、画面右側の`Add Bot`ボタンを押してください。  
+
+![写真4](images/image4.png)
+
+すると、以下のような画面になります。  
+
+![写真5](images/image5.png)
+
+次に、左のメニューバーにある`OAuth2`のタブを選択します。  
+
+![写真6](images/image6.png)
+
+上の画面の`bot`にチェックを入れ、下にスクロールしbotのパーミッションを設定します。  
+`View Channel`と`Send Messages`にチェックを入れ、画面中央右の`Copy`ボタンを押します。  
+
+![写真7](images/image7.png)
+
+コピーしたリンクをwebブラウザ上で開くと以下のような画面になります。  
+
+![写真8](images/image8.png)
+
+この画面で自分が管理者権限を持っているサーバーを選択します。  
+選択したサーバーにbotが追加されれば、botが正常にサーバーに参加することができました。  
+
+次に、botを実際に動かすためのコードを実行します。  
+botを実際に稼働推させるためには、 `TOKEN`と`CLIENT ID`が必要になります。  
+`TOKEN`はメニューバーから`Bot`を選択し、画面中央の`Copy`ボタンからコピーすることができます。  
+また、`CLIENT ID`は`General Information`タブの画面中央左の`CLIENT ID`の項目から確認することができます。  
+それぞれ以下の画像の部分から確認することができます。  
+
+
+![写真9](images/image9.png)
+
+![写真10](images/image10.png)
+
+実行するためのrubyコードを生成するシェルスクリプトを実行するために権限の変更をおこないます。  
+パスワードを聞かれるので、入力してください。  
+その後、シェルスクリプトを実行します。  
+シェルスクリプトを実行すると、`TOKEN`と`CLIENT ID`を入力する画面が出てくるので、それぞれコピーしたものを入力し、エンターを押してください。  
+以下のコマンドを実行します。  
+
+```shell
+sudo chmod 555 create_code.sh
+./create_code.sh 2 > /dev/null
+```
+`my_discord_bot.rb`というファイルの8、9行目に自分のbotの`TOKEN`と`CLIENT_ID`が入力されていれば成功です。  
+
+最後に、`bundler`というrubyのパッケージマネージャーをインストールします。  
+bundlerをインストールした後、コードを実行するために必要なライブラリをインストールしていきます。  
+そして、rubyコードを実行します。  
+
+以下のコマンドを実行してください。  
+
+```shell
+gem install bundler
+bundle init
+echo 'gem "iso_country_codes"
+gem "discordrb"
+' >> Gemfile
+bundle install --local path vendor/bundle
+bundle exec ruby my_discord_bot.rb
+```
+
+`[INFO : websocket @ 2021-03-09 19:29:24.294] Discord using gateway protocol version: 6, requested: 6`のような文言が表示されればうまく起動できています。  
+
+あとは、discordサーバー上で適切なコマンドを打てば良いだけです。  
+
+### botコマンドの仕様
+
+botは以下の`/covid`コマンドでAPIから情報を取ってきます。  
+また、`/covid`コマンドは、1つか2つの引数を受け取ります。  
+
+#### 引数が1つの場合
+
+引数が1つの場合には、引数に国名を受け取ります。  
+国名は、ISO 3166-1 alpha-3に従っています。  
+[こちら](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/iso3166-1.html)のurlを参考にしてください。  
+
+APIから昨日のデータを取ってこようとしますが、昨日のデータがまだ更新されていない場合にはAPIが保有している最新の情報を取得してきます。  
+
+（例）
+```
+/covid jpn
+```
+
+#### 引数が2つの場合
+
+引数が2つの場合には、第一引数に国名（上記と同じ）、第二引数に日付を受け取ります。  
+日付のフォーマットは、ISO 8601に従っています。  
+YYYY-MM-DDの形で渡してください。  
+
+（例）
+```
+/covid jpn 2020-07-07
+```
+
+どちらの引数もフォーマットに従っていない場合には弾かれます。  
+また、第二引数で指定した日付の情報がAPIにない場合にもエラーが表示されるようになっています。  
 
 ## 開発環境
 - MacBook Pro (15-inch, 2018)
-- macOS Big Sir Version 11.2.1
+- macOS Big Sirw Version 11.2.1
 - ruby 3.0.0p0 (2020-12-25 revision 95aff21468) [x86_64-darwin20]
---
+++ project/create_code.sh
@@ -0,0 +1,171 @@
+echo "TOKEN:"
+read TOKEN
+echo "CLIENT_ID:"
+read CLIENT_ID
+
+echo "require \"date\"
+require \"uri\"
+require \"net/https\"
+require \"json\"
+require \"discordrb\"
+require \"iso_country_codes\"
+
+TOKEN = \"$TOKEN\"
+CLIENT_ID = \"$CLIENT_ID\"
+BASE_URL = \"https://covidapi.info/api/v1\"
+
+def iso_validator(country_str)
+    ret = false
+    for country in IsoCountryCodes.all
+        if country.alpha3 == country_str.upcase
+            ret = country.alpha3
+            break
+        end
+    end
+    return ret
+end
+
+def date_validator(date_str)
+  date_ary = date_str.split(/-/)
+  if date_ary.size == 3 && Date.valid_date?(date_ary[0].to_i, date_ary[1].to_i, date_ary[2].to_i)
+     Date.parse(date_str)
+  else
+     return false
+  end
+end
+
+def single_date_func(content, country)
+    begin
+		temp_res = Net::HTTP.get_response(URI.parse(\"#{BASE_URL}/latest-date\"))
+        temp_res.value
+        if (Date.parse(\"2020-01-22\") <= Date.parse(content.date_str)) && (Date.parse(content.date_str) <= Date.parse(temp_res.body))
+          content.res = Net::HTTP.get_response(URI.parse(\"#{BASE_URL}/country/#{country}/#{content.date_str}\"))
+          content.res.value
+        else
+          content.err_msg = \"[error] date is out of range\"
+        end
+    rescue => err
+        content.err_msg = \"[error]\" + err.message
+    end
+    return content
+end
+
+def check_yesterday(content, country)
+    begin
+    content.date_str = (Date.today() - 1).to_s
+    content.res = Net::HTTP.get_response(URI.parse(\"#{BASE_URL}/country/#{conutry}/#{content.date_str}\"))
+    content.res.value
+    rescue
+        begin
+			temp_res = Net::HTTP.get_response(URI.parse(\"#{BASE_URL}/latest-date\"))
+            content.date_str = temp_res.body
+            content.res = Net::HTTP.get_response(URI.parse(\"#{BASE_URL}/country/#{country}/latest\"))
+            content.res.value 
+            content.update_ret(\"Sorry, yesterday data is not avaliable yet.\nHere is the latest data.\n\")
+        rescue => err
+            content.err_msg = \"[error]\" + err.message
+        end
+    end
+    return content
+end
+
+def make_ans(content)
+    begin
+        temp = JSON.parse(content.res.body)[\"result\"][content.date_str.to_s]
+        content.confirmed = temp[\"confirmed\"]
+        content.deaths = temp[\"deaths\"]
+        content.recovered = temp[\"recovered\"]
+    rescue 
+        content.err_msg = \"[error] JSON format is invalid.\"
+    end
+    return content
+end
+
+def main_func(content, args)
+    if (args.size() == 0) then
+        content.err_msg = \"[error] Arguments are required.\" 
+    elsif (!(country_code = iso_validator(args[0].to_s))) then
+        content.err_msg = \"[error] Country code is invalid.\"
+    elsif (args.size() == 1) then
+        content = check_yesterday(content, country_code)
+        content.err_msg ? nil : (content = make_ans(content))
+    elsif (args.size() == 2) then
+        if (!(date_validator(args[1].to_s))) then
+            content.err_msg = \"[error] The date format is invalid.\"
+        else
+            content.date_str = args[1].to_s
+            (content.err_msg) ? nil : (content = single_date_func(content, country_code))
+            (content.err_msg) ? nil : (content = make_ans(content))
+        end
+    else
+        content.err_msg = \"[error] Arguments are invalid.\"
+    end
+end
+
+class Content
+	def initialize()
+		@err_msg = nil
+		@ret_msg = nil
+		@date_str = nil
+        @confirmed = nil
+        @deaths = nil
+        @recovered = nil
+		@res = nil
+	end
+
+	attr_accessor :err_msg, :ret_msg, :res, :date_str, :confirmed, :deaths, :recovered
+
+    def update_ret(str)
+        unless (@ret_msg)
+            @ret_msg = \"\"
+        end
+		@ret_msg = @ret_msg + str
+        return @ret_msg
+    end
+end
+
+bot = Discordrb::Commands::CommandBot.new(
+    token: TOKEN,
+    client_id: CLIENT_ID,
+    prefix:'/',
+)
+
+bot.command :covid do |event, *args|
+    content = Content.new
+	main_func(content, args)
+    if (content.err_msg) then
+        event.send_embed do |embed|
+          embed.title = \"Error\"
+          embed.colour = 0xFF1F1F
+          embed.description = content.err_msg
+        end
+    else
+        event.send_embed do |embed|
+          embed.title = \"Covid INFO\"
+          embed.colour = 0x1E90FF
+          embed.description = content.ret_msg
+          embed.add_field(
+            name: \"Date:\",
+            value: content.date_str,
+            inline: false
+          )
+          embed.add_field(
+            name: \"Confirmed:\",
+            value: content.confirmed,
+            inline: true
+          )
+          embed.add_field(
+            name: \"Deaths:\",
+            value: content.deaths,
+            inline: true
+          )
+          embed.add_field(
+            name: \"Recovered:\",
+            value: content.recovered,
+            inline: true
+          )
+        end
+    end
+end
+
+bot.run" > my_discord_bot.rb
